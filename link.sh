2868d60109936c95d76eba45b6a2ebe11cf40998
Kosuke Yamada <39643260+Kosuke-Yamada@users.noreply.github.com>
2025-02-07 16:16:08 +0900

    Update link.sh

--- link.sh
+++ link.sh
@@ -42,6 +42,10 @@ git config --global user.name Kosuke-Yamada
 sudo locale-gen ja_JP.UTF-8
 sudo update-locale LANG=ja_JP.UTF-8
 
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+
 cp -r $HOME/project/.ssh $HOME/.ssh
 
 zsh
